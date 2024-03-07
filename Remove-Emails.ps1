9490b9d78674e4e0fee3f554c58a608e52a14d40
Andrew <39089438+anritschel@users.noreply.github.com>
2024-03-07 11:23:39 -0500

    Update Remove-Emails.ps1

--- Remove-Emails.ps1
+++ Remove-Emails.ps1
@@ -6,6 +6,6 @@ $query = '(Subject: '') AND (Sender: '')'
 $Search = New-ComplianceSearch -Name $searchName -ExchangeLocation All -ContentMatchQuery $query
 Start-ComplianceSearch -Identity $Search.Identity
 
-#Wait for compliance search to complete, uset get-compliancesearch -identity $Search.Identity to check status
+#Wait for compliance search to complete, uset get-compliancesearch $Search.Name to check status
 
 New-ComplianceSearchAction -SearchName $searchName -Purge -PurgeType HardDelete
